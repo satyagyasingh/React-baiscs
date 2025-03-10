1d18929499b45cc60ca82b28bb609be2699b079d
satyagyasingh <satyagyasingh@gmail.com>
2025-03-11 05:08:23 +0530

    build basic app using react router

--- 09-ReactRouter/src/App.jsx
+++ 09-ReactRouter/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import Header from './components/header'
-import Footer from "./components/footer"
-import './App.css'
+import Header from './components/Header'
+import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
      <Header/>
+     <Home/>
       <Footer/>
      <h1>system</h1>
-    </>
+    </BrowserRouter>
   )
 }
 
--- 09-ReactRouter/src/main.jsx
+++ 09-ReactRouter/src/main.jsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home.jsx'
+import Contact from './components/Contact.jsx'
+import About from './components/about.jsx'
+import User from './components/User.jsx'
+import Github, { githubInfoLoader } from './components/Github.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+// the above one is optional method but looks more complicated
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
